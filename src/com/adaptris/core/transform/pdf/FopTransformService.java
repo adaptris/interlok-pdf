047eaffe80881880013fea270d67bcc7384e82d8
Lewin Chan <lewin.chan@adaptris.com>
2013-06-27 16:49:47 +0100

    redmineID #4143, redmineID #1379 - FopTransformService moved to its own package so that core can get rid of some of its dependencies.

---
+++ src/com/adaptris/core/transform/pdf/FopTransformService.java
@@ -0,0 +1,103 @@
+package com.adaptris.core.transform.pdf;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.adaptris.annotation.MarshallingImperative;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.ServiceImp;
+
+/**
+ * Transform service based on the Apache Fop project - http://xmlgraphics.apache.org/fop/
+ * <p>
+ * In the adapter configuration file this class is aliased as <b>fop-transform-service</b> which is the preferred alternative to the
+ * fully qualified classname when building your configuration.
+ * </p>
+ */
+@XStreamAlias("fop-transform-service")
+@MarshallingImperative(mapTo="fop-transform-service", transientFields={})
+public class FopTransformService extends ServiceImp {
+
+  // marshalled
+  private String outputFormat;
+
+  // castor transient
+  private transient Transformer transformer;
+
+  /**
+   * Creates new instance.  Default output format is <code>MimeConstants.MIME_PDF</code>.
+   */
+  public FopTransformService() {
+    this.setOutputFormat(MimeConstants.MIME_PDF);
+  }
+
+  /** @see com.adaptris.core.AdaptrisComponent#init() */
+  public void init() throws CoreException {
+    try {
+      transformer = TransformerFactory.newInstance().newTransformer();
+    }
+    catch (TransformerConfigurationException e) {
+      throw new ServiceException(e);
+    }
+  }
+
+  /** @see com.adaptris.core.Service#doService(com.adaptris.core.AdaptrisMessage) */
+  public void doService(AdaptrisMessage msg) throws ServiceException {
+
+    OutputStream out = null;
+    InputStream in = null;
+    try {
+      out = msg.getOutputStream();
+      in = msg.getInputStream();
+      FopFactory fopFactory = FopFactory.newInstance();
+      Fop fop = fopFactory.newFop(this.getOutputFormat(), fopFactory.newFOUserAgent(), out);
+
+      Source source = new StreamSource(in);
+      Result result = new SAXResult(fop.getDefaultHandler());
+      this.transformer.transform(source, result);
+//      msg.setPayload(out.toByteArray());
+    }
+    catch (Exception e) {
+      throw new ServiceException(e);
+    }
+    finally {
+      IOUtils.closeQuietly(in);
+      IOUtils.closeQuietly(out);
+    }
+  }
+
+  /** @see com.adaptris.core.AdaptrisComponent#close() */
+  public void close() {
+    // n/a...
+  }
+
+  // properties
+
+  public String getOutputFormat() {
+    return outputFormat;
+  }
+
+  public void setOutputFormat(String s) {
+    if (s == null || "".equals(s)) {
+      throw new IllegalArgumentException("null or empty param");
+    }
+    this.outputFormat = s;
+  }
+}
---
+++ test/src/com/adaptris/core/transform/pdf/FopTransformServiceTest.java
@@ -0,0 +1,59 @@
+package com.adaptris.core.transform.pdf;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.adaptris.core.AdaptrisMarshaller;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.DefaultMarshaller;
+import com.adaptris.core.Service;
+import com.adaptris.core.transform.TransformServiceExample;
+import com.adaptris.core.transform.pdf.FopTransformService;
+
+public class FopTransformServiceTest extends TransformServiceExample {
+
+  private static String INPUT_KEY = "FopTransformService.inputMessage";
+  private static String OUTPUT_KEY = "FopTransformService.outputMessage";
+
+  private byte[] input;
+
+  private FopTransformService service;
+
+  public FopTransformServiceTest(String name) throws Exception {
+    super(name);
+
+    if (PROPERTIES.getProperty(INPUT_KEY) != null) {
+      FileInputStream fileInputStream = new FileInputStream(new File(PROPERTIES
+          .getProperty(INPUT_KEY)));
+
+      input = new byte[fileInputStream.available()];
+      fileInputStream.read(input);
+    }
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    service = new FopTransformService();
+  }
+
+  public void testService() throws Exception {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    msg.setPayload(input);
+    execute(service, msg);
+    assertTrue(msg.getStringPayload().startsWith("%PDF-1.4"));
+  }
+
+  @Override
+  protected Object retrieveObjectForSampleConfig() {
+    return service;
+  }
+
+  @Override
+  public void testXmlRoundTrip() throws Exception {
+    AdaptrisMarshaller m = DefaultMarshaller.getDefaultMarshaller();
+    String xml = m.marshal(service);
+    Service unmarshalledService = (FopTransformService) m.unmarshal(xml);
+    assertRoundtripEquality(service, unmarshalledService);
+  }
+}
